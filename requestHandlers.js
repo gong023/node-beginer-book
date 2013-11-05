d9ab67c53aaec9c958e74046addeb962a6addf7a
gong023 <gon.gong.gone@gmail.com>
2013-11-05 14:49:36 +0900

    bad for non-blocking2

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,12 +3,11 @@ var exec = require("child_process").exec;
 var start = function() {
   console.log("start");
 
-  var sleep = function(milli_seconds) {
-    var start_time = new Date().getTime();
-    while(new Date().getTime() < start_time + milli_seconds);
-  }
-  sleep(10000);
-  return "Hello Start";
+  var content = "empty";
+  exec("ls -lah", function (error, stdout, stderr) {
+    content = stdout;
+  });
+  return content;
 }
 
 var upload = function() {
